17fa13e2bdcdd4326fac91af69cf7065c7dcb08b
sachin_07 <114389668+sachinaugi07@users.noreply.github.com>
2026-04-01 11:31:51 +0530

    cors enabled

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,11 @@ import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nes
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   // app.useLogger(app.get(Logger)); // Use pino logger
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
