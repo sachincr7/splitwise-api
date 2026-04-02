ece2df21bef1064cac6db92e515493d51bc02c25
sachin_07 <114389668+sachinaugi07@users.noreply.github.com>
2026-04-02 12:48:40 +0530

    created roles decorator

--- apps/api/src/entities/user.entity.ts
+++ apps/api/src/entities/user.entity.ts
@@ -2,6 +2,7 @@ import { Exclude } from 'class-transformer';
 import { BaseEntityClass } from './base.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { ExpenseGroupEntity } from './expense-group.entity';
+import { Role } from './enums/role.enum';
 
 @Entity('users')
 export class UserEntity extends BaseEntityClass {
@@ -17,6 +18,13 @@ export class UserEntity extends BaseEntityClass {
   @Exclude()
   @Column()
   password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
 
   @Exclude()
   @OneToMany(() => ExpenseGroupEntity, (group) => group.owner)
